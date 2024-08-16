c0fb73367f4be50218674f1af4360122e7b63377
Sreeman-23 <sreemansowmi@gmail.com>
2024-08-16 11:20:33 +0530

    Update page turner.js

--- page turner.js
+++ page turner.js
@@ -1,33 +1,56 @@
-var a=document.querySelector(".six")
-var b=document.querySelector(".seven")
-var c=document.getElementById("five")
-c.addEventListener("click",function(){
-    a.style.display="block"; 
-    b.style.display="block"; 
-})
-var d=document.getElementById("cancel")
-d.addEventListener("click",function(event){
-    event.preventDefault()
-    a.style.display="none"; 
-    b.style.display="none"; 
-})
-var e=document.querySelector(".two");
-var f=document.getElementById("add");
-var g=document.getElementById("book-title");
-var h=document.getElementById("book-author");
-var i=document.getElementById("book-description");
-f.addEventListener("click",function(event){
+// Select DOM elements
+var overlay = document.querySelector(".six");
+var popup = document.querySelector(".seven");
+var showPopupButton = document.getElementById("five");
+var cancelButton = document.getElementById("cancel");
+var addButton = document.getElementById("add");
+var bookTitleInput = document.getElementById("book-title");
+var bookAuthorInput = document.getElementById("book-author");
+var bookDescriptionInput = document.getElementById("book-description");
+var bookContainer = document.querySelector(".two");
+
+// Show the popup and overlay when the "+" button is clicked
+showPopupButton.addEventListener("click", function() {
+    overlay.style.display = "block";
+    popup.style.display = "block";
+});
+
+// Hide the popup and overlay when the "CANCEL" button is clicked
+cancelButton.addEventListener("click", function(event) {
     event.preventDefault();
-    var div=document.createElement("div")
-    div.setAttribute("class","three")
-    div.innerHTML=`<h2>${g.value}</h2>
-          <h4>${h.value}</h4>
-          <p>${i.value}
-          </p>
-        <button class="ten" onclick="deletebook(event)">Delete</button></div>`
-    e.append(div)
-    a.style.display="none"; 
-    b.style.display="none"; })
-function deletebook(event){
-    event.target.parentElement.remove()
+    overlay.style.display = "none";
+    popup.style.display = "none";
+});
+
+// Add a new book to the list when the "ADD" button is clicked
+addButton.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    // Create a new book entry
+    var bookDiv = document.createElement("div");
+    bookDiv.className = "three"; // Using className instead of setAttribute
+
+    bookDiv.innerHTML = `
+        <h2>${bookTitleInput.value}</h2>
+        <h4>By: ${bookAuthorInput.value}</h4>
+        <p>${bookDescriptionInput.value}</p>
+        <button class="ten" onclick="deletebook(event)">Delete</button>
+    `;
+
+    // Append the new book entry to the container
+    bookContainer.appendChild(bookDiv);
+
+    // Clear input fields
+    bookTitleInput.value = '';
+    bookAuthorInput.value = '';
+    bookDescriptionInput.value = '';
+
+    // Hide the popup and overlay
+    overlay.style.display = "none";
+    popup.style.display = "none";
+});
+
+// Function to delete a book entry
+function deletebook(event) {
+    event.target.parentElement.remove();
 }
